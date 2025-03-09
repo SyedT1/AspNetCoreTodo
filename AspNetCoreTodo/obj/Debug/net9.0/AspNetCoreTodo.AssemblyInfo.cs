00a528d9f4b7433244616d87d3da4e5dc1d30b72
Syed Mohaiminul Hoque <syedmuhaimintahsin@gmail.com>
2025-03-09 11:13:15 +0600

    added todocontroller

--- AspNetCoreTodo/obj/Debug/net9.0/AspNetCoreTodo.AssemblyInfo.cs
+++ AspNetCoreTodo/obj/Debug/net9.0/AspNetCoreTodo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa5f01588117d5d018532677332fb31810a392a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
